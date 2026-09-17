dec4e67345f940952f82b5252fdac63869896ad9
Nawid Azimi <nawidahmadrasooli06@gmail.com>
2026-09-17 12:54:08 +0200

    Add environment variables and creator info in config

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+CREATOR_NAME = "〘Cactuc = نــوید〙"
+CREATOR_USERNAME = "cactuc580"
+CREATOR_ID_LINK = f"https://t.me/{CREATOR_USERNAME}"
+DEFAULT_LANG = "fa"
+SUPPORTED_LANGS = ["fa", "en", "ru", "ar"]
 ADMIN_ID = 8659480577
+
